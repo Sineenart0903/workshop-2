6b7d5b86a9b19ba01862caaee2063769916a5f83
Sineenart0903 <moon_10700@hotmail.com>
2021-02-06 22:31:22 +0700

    Add new exercise (while/for)

--- while_for_exercise1.py
+++ while_for_exercise1.py
@@ -10,10 +10,10 @@ while i <= 12:
     i += 1
 # for
 i = 2
-for i in range(2, 12):
+for i in range(2, 13):
     j = 1
     print("  [", i, "]")
-    for j in range(2, 12):
+    for j in range(2, 13):
         print(i, "*", j, ":", i * j, end="\n")
         j += 1
     print("---------------")
